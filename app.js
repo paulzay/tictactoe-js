033cf7bdc80b8a1602177eae8281ee5911dda089
paulzay <paulotieno2@gmail.com>
2020-10-06 13:15:44 +0300

    check the winning condition and display winner or draw message

--- app.js
+++ app.js
@@ -24,6 +24,69 @@ const gameBoard = (() => {
 	const drawMessage = () => `It is a draw`;
 	gameOn = true;
 
+
+	const resetGame = () => {
+	    location.reload()
+	}
+	document.querySelector('.game-reset').addEventListener('click', resetGame);
+
+	const clearBoard = () =>{
+		gameOn = true;
+		let currentPlayerMarker = player1.marker;
+		let currentPlayerName = player1.name;
+		board = ["", "", "", "", "", "", "", "", ""];
+		messageBoard.innerHTML = currentPlayerTurn();
+		document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '')
+	}
+	document.querySelector('.clear-board').addEventListener('click', clearBoard);
+
+	const takeTurns = () =>{
+		currentPlayerMarker = currentPlayerMarker === 'X'? 'O' : 'X';
+		currentPlayerName = currentPlayerName === player1.name ? player2.name : player1.name;
+		messageBoard.innerHTML = currentPlayerTurn();
+	}
+
+
+  const checkWinner = () => {
+  	let winner = false;
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    let result = winConditions.some((indices) => {
+      return (
+        board[indices[0]] == currentPlayerMarker &&
+        board[indices[1]] == currentPlayerMarker &&
+        board[indices[2]] == currentPlayerMarker
+      );
+    });
+
+    if (result == true) {
+      winner = true;
+      messageBoard.innerHTML = winningMessage();
+      gameOn = false;
+
+      return;
+
+    }
+
+
+    if (!board.some((e) => e === "") && winner == false) {
+        messageBoard.innerHTML = drawMessage();
+      	gameOn = false;
+      	return;
+
+    }
+
+    takeTurns();
+  }
+
 	const placeMarker = (clickedCell, clickedCellIndex) => {
 	  	board[clickedCellIndex] = currentPlayerMarker;
 	    clickedCell.innerHTML = currentPlayerMarker;
@@ -37,33 +100,11 @@ const gameBoard = (() => {
 	        return;
 	    }
 	    placeMarker(clickedCell, clickedCellIndex);
-	    takeTurns();
+	    checkWinner();
 	};
 
-	const resetGame = () => {
-	    location.reload()
-	}
-	document.querySelector('.game-reset').addEventListener('click', resetGame);
-
-	const clearBoard = () =>{
-		gameOn = true;
-		let currentPlayerMarker = player1.marker;
-		let currentPlayerName = player1.name;
-		board = ["", "", "", "", "", "", "", "", ""];
-		document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '')
-	}
-	document.querySelector('.clear-board').addEventListener('click', clearBoard);
-	// const play = (target, marker) => {
-	// 	board[target] = currentPlayerMarker;
-	// }
 	document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', checkCellClick));
 
-	const takeTurns = () =>{
-
-		currentPlayerMarker = currentPlayerMarker === 'X'? 'O' : 'X';
-		currentPlayerName = currentPlayerName === player1.name ? player2.name : player1.name;
-
-	}
 
 })();
 
